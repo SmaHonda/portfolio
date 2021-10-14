b5004d2607f45285085fd3bcbd672035f0fdf7f7
SmaHonda <86571238+SmaHonda@users.noreply.github.com>
2021-10-14 19:59:37 +0800

    V2

---
+++ javascript/javascript.js
@@ -0,0 +1,47 @@
+$('#nav_about').click( function(){
+    $('#target_about').css('display','flex')
+    $('#target_skill').css('display','none')
+    $('#target_myWeb').css('display','none')
+})
+$('#nav_skill').click( function(){
+    $('#target_about').css('display','none')
+    $('#target_skill').css('display','flex')
+    $('#target_myWeb').css('display','none')
+})
+$('#nav_myWeb').click( function(){
+    $('#target_about').css('display','none')
+    $('#target_skill').css('display','none')
+    $('#target_myWeb').css('display','flex')
+})
+
+$('#list_img_fansdessert').click(function(){
+    $('#fansdessert').css('display','flex')
+    $('#selectNav').css('display','none')
+})
+$('#list_fansdessert').click(function(){
+    $('#fansdessert').css('display','flex')
+    $('#selectNav').css('display','none')
+})
+$('#list_img_mycalc').click(function(){
+    $('#mycalc').css('display','flex')
+    $('#selectNav').css('display','none')
+})
+$('#list_mycalc').click(function(){
+    $('#mycalc').css('display','flex')
+    $('#selectNav').css('display','none')
+})
+$('#list_img_fuji').click(function(){
+    $('#fuji').css('display','flex')
+    $('#selectNav').css('display','none')
+})
+$('#list_fuji').click(function(){
+    $('#fuji').css('display','flex')
+    $('#selectNav').css('display','none')
+})
+
+$('.BackBtn').click(function(){
+    $('#selectNav').css('display','flex')
+    $('#fansdessert').css('display','none')
+    $('#mycalc').css('display','none')
+
+})
